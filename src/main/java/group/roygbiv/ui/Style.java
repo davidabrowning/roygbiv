146861613aed7cf6ba733e72387aa12e94456663
David <11443294+davidabrowning@users.noreply.github.com>
2024-02-28 14:30:21 -0500

    Define static BORDER_RADIUS and BACKGROUND_RADIUS variables. Set CornerRadii for Borders and Backgrounds.

--- src/main/java/group/roygbiv/ui/Style.java
+++ src/main/java/group/roygbiv/ui/Style.java
@@ -10,7 +10,15 @@ public class Style {
     public static int CARD_HEIGHT = 120;
     public static int CARD_WIDTH = 100;
 
-    public static Border unhighlightedBorder = new Border(new BorderStroke(Color.WHITESMOKE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(5)));
-    public static Border highlightedBorder = new Border(new BorderStroke(Color.LIGHTBLUE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(5)));
+    public static int BORDER_RADIUS = 10;
+    public static int BACKGROUND_RADIUS = 10;
+
+    public static Border unhighlightedBorder = new Border(new BorderStroke(Color.WHITESMOKE, BorderStrokeStyle.SOLID, new CornerRadii(BORDER_RADIUS), new BorderWidths(5)));
+    public static Border highlightedBorder = new Border(new BorderStroke(Color.LIGHTBLUE, BorderStrokeStyle.SOLID, new CornerRadii(BORDER_RADIUS), new BorderWidths(5)));
+
+    public static Background getBackground(Color c) {
+        BackgroundFill bf = new BackgroundFill(c, new CornerRadii(BORDER_RADIUS + BACKGROUND_RADIUS), new Insets(0, 0, 0, 0));
+        return new Background(bf);
+    }
 
 }
