bf174453f2942136328b7f8f267081e771389822
David <11443294+davidabrowning@users.noreply.github.com>
2024-03-01 12:42:19 -0500

    Add comments

--- src/main/java/group/roygbiv/domain/Player.java
+++ src/main/java/group/roygbiv/domain/Player.java
@@ -5,10 +5,15 @@ public class Player {
     private final String name;
     private final Hand hand;
 
+    // Constructor
     public Player(String name) {
         this.name = name;
         this.hand = new Hand();
     }
+
+    // ---------------------------------------------------------------------------------------------------------------
+    // Getters, setters, and toString
+
     public Hand getHand() { return hand; }
 
     @Override
