64f26728bfb3a165708d764e9ce2199473a68b34
David <11443294+davidabrowning@users.noreply.github.com>
2024-03-01 12:47:29 -0500

    Add comments

--- src/main/java/group/roygbiv/GameLauncher.java
+++ src/main/java/group/roygbiv/GameLauncher.java
@@ -6,6 +6,8 @@ import javafx.application.Application;
 
 public class GameLauncher {
 
+    // Main method is the entry point for the program
+    // and calls this class' go() method
     public static void main(String[] args) {
 
         System.out.println("Starting program.");
@@ -13,6 +15,7 @@ public class GameLauncher {
         System.out.println("Ending program.");
     }
 
+    // This method launches the application (RoygbivApplication class)
     private void go() {
         Application.launch(RoygbivApplication.class);
     }
